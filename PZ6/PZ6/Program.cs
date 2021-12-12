f952fe203424b6a628ffa2c278391c7ef8b4076e
Daniil-Shmarin <93378092+Daniil-Shmarin@users.noreply.github.com>
2021-12-12 17:46:46 +0500

    Add files via upload

---
+++ PZ6/PZ6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PZ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.8м/с в квадрат, каждые 0.5  v=gt
+            double t = Double.Parse(Console.ReadLine());
+            double i=0;
+            i = t;
+            while (i >0)
+            {
+                Console.WriteLine(t * 4.9);
+                t = t + 0.5;
+                i = i - 0.5;
+            }
+
+        }
+
+    }
+}
